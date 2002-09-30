b22676afdf5cb21eef4df49cb713e530f5a7f5d9
Paul Eggert <eggert@cs.ucla.edu>
2002-09-30 09:15:02 +0000

    gzip 1.3.5

--- revision.h
+++ revision.h
@@ -1,12 +1,12 @@
 /* revision.h -- define the version number
+ * Copyright (C) 1998, 1999, 2001, 2002 Free Software Foundation, Inc.
  * Copyright (C) 1992-1993 Jean-loup Gailly.
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License, see the file COPYING.
  */
 
-#define VERSION "1.2.4"
 #define PATCHLEVEL 0
-#define REVDATE "18 Aug 93"
+#define REVDATE "2002-09-30"
 
 /* This version does not support compression into old compress format: */
 #ifdef LZW
