b2f9e266fc08ea910be5cdb0744ff1805699c81f
Jim Meyering <meyering@redhat.com>
2009-09-06 10:37:32 +0200

    maint: update build-from-scratch infrastructure
    * bootstrap: Modernize.
    * bootstrap.conf: Modernize.
    * cfg.mk: New file.

---
+++ cfg.mk
@@ -0,0 +1,42 @@
+# Customize maint.mk                           -*- makefile -*-
+# Copyright (C) 2003-2009 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Use alpha.gnu.org for alpha and beta releases.
+# Use ftp.gnu.org for major releases.
+gnu_ftp_host-alpha = alpha.gnu.org
+gnu_ftp_host-beta = alpha.gnu.org
+gnu_ftp_host-major = ftp.gnu.org
+gnu_rel_host = $(gnu_ftp_host-$(RELEASE_TYPE))
+
+# Used in maint.mk's web-manual rule
+manual_title = gzip: the data compression program
+
+url_dir_list = \
+  ftp://$(gnu_rel_host)/gnu/coreutils
+
+# The GnuPG ID of the key used to sign the tarballs.
+gpg_key_ID = B9AB9A16
+
+# Tests not to run as part of "make distcheck".
+local-checks-to-skip = strftime-check
+
+# Tools used to bootstrap this package, used for "announcement".
+bootstrap-tools = autoconf,automake,gnulib
+
+# Now that we have better tests, make this the default.
+export VERBOSE = yes
+
+old_NEWS_hash = d41d8cd98f00b204e9800998ecf8427e
