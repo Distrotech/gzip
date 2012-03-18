0a284baeaedca68017f46d2646e4c921aa98a90d
Paul Eggert <eggert@cs.ucla.edu>
2012-03-18 11:07:02 -0700

    gzip: fix nondeterministic compression results
    Reported by Jakub Wilk in <http://bugs.debian.org/647522>.
    * deflate.c (fill_window): Don't let garbage pollute the dictionary.

--- deflate.c
+++ deflate.c
@@ -571,6 +571,8 @@ local void fill_window()
         n = read_buf((char*)window+strstart+lookahead, more);
         if (n == 0 || n == (unsigned)EOF) {
             eofile = 1;
+            /* Don't let garbage pollute the dictionary.  */
+            memzero (window + strstart + lookahead, MIN_MATCH - 1);
         } else {
             lookahead += n;
         }
